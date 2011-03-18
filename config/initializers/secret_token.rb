9469e9d533d22adb068b968072409c32181dc895
Sanjay Paul <paul.sanjaynj@gmail.com>
2011-03-18 18:36:52 -0400

    Initialization (Sanjay)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Penntwit::Application.config.secret_token = '7e3d960c4778917863874d079e67a852726a0ae9e21c8541598417c439014cf0ce10d3c89ccd0f8dcb9f67e900e15824310794b513763477839360e96df4179d'
