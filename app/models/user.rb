862b310a315527514bf2fcba71e304b650dfb729
Sally Guo <enchanting.phoenix@gmail.com>
2011-03-20 16:58:11 -0400

    Did almost everything in the tutorials up to and including chapter 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
 has_many :microposts
+attr_accessible :firstname, :lastname, :email, :location, :timezone, :bio, :privacy, :webpage
+email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+validates :firstname, :presence => true
+validates :email, :presence => true,
+                  :format   => { :with => email_regex }
 end
