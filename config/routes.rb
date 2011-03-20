821471d276430fd1e68749348bbb087bde838600
Sally Guo <enchanting.phoenix@gmail.com>
2011-03-21 04:58:11 +0800

    Did almost everything in the tutorials up to and including chapter 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Penntwit::Application.routes.draw do
+
+  get "users/new"
+  match '/signup', :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
 
