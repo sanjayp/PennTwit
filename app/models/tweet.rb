187a5d7ba868b5e693ce42a28272ea711dfc7392
Sally Guo <enchanting.phoenix@gmail.com>
2011-03-20 10:20:59 +0800

    Removed default Rails page

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,2 +1,5 @@
 class Tweet < ActiveRecord::Base
+belongs_to :user
+
+validates :content, :length => { :maximum => 140 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Penntwit::Application.routes.draw do
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+
+  root :to => 'pages#home'
+
+ get "pages/home"
+
+ get "pages/contact"
+
+ get "pages/about"
+
   resources :messages
 
   resources :tweets
