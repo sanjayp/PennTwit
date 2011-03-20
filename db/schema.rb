2b91a4d1ae7bcbc5e18a2c1d9ff1641825a52edc
Sally Guo <enchanting.phoenix@gmail.com>
2011-03-21 04:58:11 +0800

    Did almost everything in the tutorials up to and including chapter 7

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110318230108) do
+ActiveRecord::Schema.define(:version => 20110320153654) do
 
   create_table "messages", :force => true do |t|
     t.string   "content"
@@ -40,5 +40,7 @@ ActiveRecord::Schema.define(:version => 20110318230108) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
